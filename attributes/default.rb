2545910d3e939fa5fe3f6cfbc6ac5e16e1ed8c8a
m4udev <drixson.osena@machines4u.com.au>
2015-12-18 08:20:44 +0800

    Update default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,3 +2,5 @@ default['site']['user'] = 'web_admin'
 default['site']['group'] = 'web_admin'
 
 default['site']['document_root'] = '/var/www'
+
+default['awesome_customers']['enabled_firewall_rules'] = %w(firewall_http firewall_sshd)
