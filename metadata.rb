147c7ee8f1fcce6a4c3202bb151c0261370015df
m4udev <drixson.osena@machines4u.com.au>
2015-12-18 10:29:38 +0800

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,8 @@ name "opsworks_cookbook_demo"
 #depends 'selinux', '~> 0.9.0'
 #depends 'iptables', '~> 1.0.0'
 
-depends 'httpd'
-depends 'selinux'
-depends 'iptables'
+source "https://supermarket.chef.io"
+
+cookbook 'httpd', '>= 1.4.2', git: 'https://github.com/chef-cookbooks/httpd.git'
+cookbook 'selinux', '>= 1.4.2', git: 'https://github.com/skottler/selinux.git'
+cookbook 'iptables', '>= 1.4.2', git: 'https://github.com/chef-cookbooks/iptables.git'
