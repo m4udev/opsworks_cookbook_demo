ce9750956c2e4a6b09b9c61fe8cf8f4a921a0fe7
m4udev <drixson.osena@machines4u.com.au>
2015-12-18 08:33:00 +0800

    Update webserver.rb

--- recipes/webserver.rb
+++ recipes/webserver.rb
@@ -17,8 +17,29 @@ end
 #end
 
 #write the homepage
-file '/var/www/index.php' do 
+file '/var/www/index.html' do 
   content '<html>This is a web</html>'
+  mode '0644'
+  owner 'web_admin'
+  group 'web_admin'
+end
+
+# Install apache , config and etc END
+
+# Install the mod_php5 apache module
+httpd_module 'php' do 
+  instance 'site'
+end
+
+#install php5-mysql
+package 'php-mysql' do 
+  action :install
+  notifies :restart, 'httpd_service[site]'
+end
+
+#write the homepage
+file '/var/www/index2.php' do 
+  content '<html><?php echo phpinfo(); ?></html>'
   mode '0644'
   owner 'web_admin'
   group 'web_admin'
