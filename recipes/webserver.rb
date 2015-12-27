819b040f4193c0cb24ff94e977541161301dba07
Drixson Oseña <drixson.osena@machines4u.com.au>
2015-12-28 07:53:20 +0800

    commit

--- recipes/webserver.rb
+++ recipes/webserver.rb
@@ -1,44 +1,48 @@
+yum_package 'httpd' do 
+  action :remove
+end
+
 yum_package 'httpd-tools' do 
   action :remove
 end
 
 # Install httpd
-yum_package 'httpd' do 
-  action :upgrade
-  version '2.4'
-end
+#yum_package 'httpd' do 
+#  action :upgrade
+#  version '2.4'
+#end
 
-service 'httpd' do
-  action :start
-end
+##service 'httpd' do
+#  action :start
+#end
 
 # Install apache , config and etc END
-yum_package 'php' do
-  action :upgrade
-  version '5.5.12'
-end
+#yum_package 'php' do
+#  action :upgrade
+#  version '5.5.12'
+#end
 
-yum_package 'php-pdo' do
-  action :install
-end
+#yum_package 'php-pdo' do
+#  action :install
+#end
 
-yum_package 'php-pear' do
-  action :install
-end
+#yum_package 'php-pear' do
+#  action :install
+#end
 
-yum_package 'php-mbstring' do
-  action :install
-end
+#yum_package 'php-mbstring' do
+#  action :install
+#end
 
 #write the homepage
-file '/var/www/html/index.php' do 
-  content '<?php echo phpinfo(); ?>'
-  mode '0755'
+#file '/var/www/html/index.php' do 
+#  content '<?php echo phpinfo(); ?>'
+#  mode '0755'
   #owner 'www'
   #group 'www'
-end
+#end
 
 
-service 'httpd' do
-  action :restart
-end
+#service 'httpd' do
+#  action :restart
+#end
