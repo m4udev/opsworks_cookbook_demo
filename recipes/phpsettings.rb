e53781860b0407777ccd85e22e8d147daf1a287e
Drixson Oseña <drixson.osena@machines4u.com.au>
2015-12-28 09:59:18 +0800

    php settings

--- recipes/phpsettings.rb
+++ recipes/phpsettings.rb
@@ -5,7 +5,7 @@
 
 ##cookbook file source will check in /files directory
 cookbook_file 'phpini settings' do 
-	action :create
+	action :create #if exists it will update see doc
 	source 'php.ini'
 	path '/etc/php.ini'
 end
