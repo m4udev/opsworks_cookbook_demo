4499a59f27e8ca2cfa9956776a80514dbc5d69f6
Drixson Oseña <drixson.osena@machines4u.com.au>
2015-12-28 13:16:16 +0800

    change create -> install

--- recipes/apachesettings.rb
+++ recipes/apachesettings.rb
@@ -1,6 +1,6 @@
 ##cookbook file source will check in /files directory
 cookbook_file 'apache settings' do 
-	action :create #if exists it will update see doc
+	action :install #if exists it will update see doc
 	source 'httpd.conf'
 	path '/etc/httpd/conf/httpd.conf'
 end
--- recipes/phpsettings.rb
+++ recipes/phpsettings.rb
@@ -1,6 +1,6 @@
 ##cookbook file source will check in /files directory
 cookbook_file 'phpini settings' do 
-	action :create #if exists it will update see doc
+	action :install #if exists it will update see doc
 	source 'php.ini'
 	path '/etc/php.ini'
 end
@@ -9,15 +9,15 @@ end
 ##install php extensions
 #GD
 yum_package 'php55-gd' do
-	action :create
+	action :install
 end
 
 #mcrypt
 yum_package 'php55-mcrypt' do
-	action :create
+	action :install
 end
 
 #memcache
 yum_package 'php55-pecl-memcached' do
-	action :create
+	action :install
 end
