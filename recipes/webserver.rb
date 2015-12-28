e1ae31140a90b3450dc74bd52c3868c2989de69f
Drixson Oseña <drixson.osena@machines4u.com.au>
2015-12-28 13:59:38 +0800

    chef commit

--- recipes/webserver.rb
+++ recipes/webserver.rb
@@ -42,13 +42,17 @@ yum_package 'php55' do
   action :install
 end
 
-yum_package 'php-mysqli' do
+#yum_package 'php-mysqli' do
+#  action :install
+#end
+
+yum_package 'php55-mysqlnd' do
   action :install
 end
 
-#yum_package 'php55-pdo' do
-#  action :install
-#end
+yum_package 'php55-pdo' do
+  action :install
+end
 
 #yum_package 'php-pear' do
 #  action :install
