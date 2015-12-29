a11f91289d86b3d1644f7cc498e110af38b874ab
Drixson Oseña <drixson.osena@machines4u.com.au>
2015-12-29 14:15:31 +0800

    chef commit

--- recipes/groups.rb
+++ recipes/groups.rb
@@ -4,17 +4,20 @@ group 'www' do
 	append true
 end
 
-
-#directory '/var/www' do
-	#action :create
-	#owner 'root'
-	#group 'www'
-	#mode '0775'
-	#recursive true
-#end
-
-execute "chown-root-www" do
-  command "chown -R root:www /var/www"
-  user "root"
+execute 'chown-root-www' do
+  command 'chown -R root:www /var/www'
+  user 'root'
   action :run
+end
+
+execute 'set-future-folders'
+	command 'find /var/www -type d -exec sudo chmod 0775 {} +'
+	user 'root'
+	action :run
+end
+
+execute 'set-future-files'
+	command 'find /var/www -type f -exec sudo chmod 0775 {} +'
+	user 'root'
+	action :run
 end
